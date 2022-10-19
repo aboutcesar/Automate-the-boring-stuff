179d59d8c7ca9c40c64ab19859005f6a4e9b4ca7
Cesar Gutierrez <cgutierrez169@gmail.com>
2022-10-19 12:55:14 -0500

    Updated stopwatch

--- Chapter 17 - Keeping time, scheduling and Launching Programs/SuperStopWatch.py
+++ Chapter 17 - Keeping time, scheduling and Launching Programs/SuperStopWatch.py
@@ -3,20 +3,22 @@ import time
 def stopwatch():
     print("Press enter to start the stop watch, and press enter to 'click' the stopwatch. Press Ctl + C to quit. ")
     input()
-    print('Time has Started')
+    print("Time has Started")
     starttime = time.time()
     lasttime = starttime  #last time tracked for the lap
     lapnum = 1
 
     #Tracking lap times
     try:
-        input()
-        laptime = round(time.time() - lasttime, 2)
-        totaltime = round(time.time() - lasttime, 2)
-        lapnum += 1
-        lasttime = time.time()  # Need time to reset to keep track of the time between laps
-
-    except:
+        while True: 
+            input()
+            laptime = round(time.time() - lasttime, 2)
+            totaltime = round(time.time() - starttime, 2)
+            print('Lap #%s: %s (%s)' % (lapnum, totaltime, laptime), end = '')
+            lapnum += 1
+            lasttime = time.time()  # Need time to reset to keep track of the time between laps
+           
+    except KeyboardInterrupt:
         #Handles the Ctl-C button and finishes the program.
         print('\nDone')
 
