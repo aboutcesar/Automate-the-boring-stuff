da13a7e85dfed253bb60e2c34badfe66f6ec337c
Cesar Gutierrez <cgutierrez169@gmail.com>
2022-10-24 10:22:32 -0500

    pyperclip stopwatch

--- Chapter 17 - Keeping time, scheduling and Launching Programs/SuperStopWatch.py
+++ Chapter 17 - Keeping time, scheduling and Launching Programs/SuperStopWatch.py
@@ -1,4 +1,4 @@
-import time
+import time, pyperclip
 
 def stopwatch():
     print("Press enter to start the stop watch, and press enter to 'click' the stopwatch. Press Ctl + C to quit. ")
@@ -14,13 +14,16 @@ def stopwatch():
             input()
             laptime = round(time.time() - lasttime, 2)
             totaltime = round(time.time() - starttime, 2)
-            print('Lap #%s: %s (%s)' % (lapnum, totaltime, laptime), end = '')
+            info = 'Lap #%s: %s (%s)' % (str(lapnum).rjust(2), str(totaltime).center(10), str(laptime).rjust(6), end = '')
+            print(info)
             lapnum += 1
             lasttime = time.time()  # Need time to reset to keep track of the time between laps
            
     except KeyboardInterrupt:
         #Handles the Ctl-C button and finishes the program.
+        pyperclip.copy(info)
         print('\nDone')
+        print('info in the clipboard')
 
 stopwatch()
 
